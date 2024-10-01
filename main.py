a8040550df786cd857d24c565f4f5a13423be5be
Fahad <fahad.alabdulkarim5@gmail.com>
2024-10-01 10:48:53 +0300

    first py file

---
+++ main.py
@@ -0,0 +1,49 @@
+#using print() function to output(display) data on the terminal 
+print("hello ")
+
+print(1+2)
+
+#defining a variable
+name = "Ahmed"
+
+#defining a variable with type hinting
+age:int = 24
+
+#camelCase : used for naming variables, functions
+employeeCity = "Riyadh"
+#snake_case / c style : used for naming variables, functions
+employee_city = "Mekkah"
+
+#PascalCase : used in classes, enums , etc.
+
+
+print(name)
+print(age)
+
+#assigning one value to multiple variables
+var1 = var2 = var3 = 25
+
+print(var2, var3)
+
+#assigning multiple values to multiple variables
+var1, var2 = 30, 40
+print(var1, var2)
+
+
+number_string = "65"
+
+#we use type() to know the type of the object
+print(type(number_string))
+
+#converting / Casting from one type to the other
+addition_result = 5 + int(number_string)
+
+print(addition_result)
+
+
+#One line comment
+
+'''
+multiline
+comment
+'''
